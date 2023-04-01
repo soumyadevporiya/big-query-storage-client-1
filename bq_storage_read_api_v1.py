12302b30c4f86556675e8e5fe6d56c2322eba686
Soumyadev Desktop <soumyadev.poriya@gmail.com>
2023-04-01 21:50:56 +0530

    partition changed

--- bq_storage_read_api_v1.py
+++ bq_storage_read_api_v1.py
@@ -30,7 +30,7 @@ read_options = ReadSession.TableReadOptions(
               selected_fields=["id", "name"]
     )
 #read_options.row_restriction = "partition_field like '%INSBI1%'"
-read_options.row_restriction = "partition_field BETWEEN 0 AND 1"
+read_options.row_restriction = "partition_field = 1"
 
 parent = "projects/{}".format(project_id_billing)
 
